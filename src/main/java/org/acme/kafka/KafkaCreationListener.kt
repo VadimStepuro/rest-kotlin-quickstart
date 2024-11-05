8fca0f0a6a3101a11b96aa73573412f94737695c
Maksim Surmach <Maksim.Surmach@x5.ru>
2024-11-05 10:46:34 +0300

    Fixed bugs

--- src/main/java/org/acme/kafka/KafkaCreationListener.kt
+++ src/main/java/org/acme/kafka/KafkaCreationListener.kt
@@ -9,9 +9,12 @@ import org.acme.storage.StorageEntityStorageService
 import org.eclipse.microprofile.reactive.messaging.Incoming
 import org.eclipse.microprofile.reactive.messaging.Message
 import java.util.concurrent.CompletionStage
+import org.jboss.logging.Logger
 
 @ApplicationScoped
 class KafkaCreationListener {
+
+    private val logger = Logger.getLogger(KafkaCreationListener::class.java);
 
     @Inject
     lateinit var eventMapper: CreationEventMapper;
@@ -22,10 +25,15 @@ class KafkaCreationListener {
 
     @Incoming("creation-event")
     fun consumeCreationEvent(message: Message<String>) : CompletionStage<Void> {
+
+        logger.info("Starting processing message : $message")
         val event = Json.decodeFromString<CreationEvent>(message.payload);
 
         val storageModel = eventMapper.mapToStorageModel(event);
         storageService.save(storageModel);
+        logger.info("Saved storage model $storageModel")
+
+        logger.info("Notifying other systems about storing model")
         notifier.notifyCreation(event);
 
         return message.ack();
--- src/main/java/org/acme/kafka/KafkaCreationNotifier.kt
+++ src/main/java/org/acme/kafka/KafkaCreationNotifier.kt
@@ -1,10 +1,14 @@
 package org.acme.kafka
 
+
 import jakarta.enterprise.context.ApplicationScoped
 import jakarta.inject.Inject
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
 import org.acme.model.CreationEvent
-import org.acme.model.CreationNotifyEvent
 import org.acme.service.mapper.CreationEventMapper
+import org.eclipse.microprofile.reactive.messaging.Channel
+import org.eclipse.microprofile.reactive.messaging.Emitter
 
 @ApplicationScoped
 class KafkaCreationNotifier {
@@ -12,7 +16,14 @@ class KafkaCreationNotifier {
     @Inject
     lateinit var creationEventMapper: CreationEventMapper;
 
-    fun notifyCreation(messageToNotify: CreationEvent) : CreationNotifyEvent {
-        return creationEventMapper.mapToNotifyEvent(messageToNotify);
+    @Inject
+    @Channel("creation-notify")
+    private lateinit var notifyEmitter: Emitter<String>;
+
+    fun notifyCreation(messageToNotify: CreationEvent) {
+        val event = creationEventMapper.mapToNotifyEvent(messageToNotify);
+        val messagePayload = Json.encodeToString(event);
+
+        notifyEmitter.send(messagePayload);
     }
 }
